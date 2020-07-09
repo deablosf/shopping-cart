074233fef81a9334bc890738f77e4d18a9d73e5d
Student_PSXGA136 <deablosf@hotmail.com>
2020-07-09 15:46:55 -0400

    here

---
+++ src/components/CartSum.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import Items from './Items'
+
+class CartSum extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+
+export default CartSum
--- src/components/Items.js
+++ src/components/Items.js
@@ -1,13 +1,35 @@
 import React, { Component } from 'react'
 
 class Items extends Component {
-    const products = 
     render() {
-        return (
-            <div>
-                
-            </div>
-        )
+        const products = [
+            {
+                name: 'Razer CPU',
+                price: 199.95,
+                Quantity: 0
+            },
+            {
+                name: 'ATLUS MotherBoard',
+                price: 96.95,
+                Quantity: 0
+            },
+            {
+                name: 'NVidia',
+                price: 165.95,
+                Quantity: 0
+            },
+            {
+                name: 'Ram',
+                price: 85.95,
+                Quantity: 0
+            },
+            {
+                name: 'SSD',
+                price: 345.95,
+                Quantity: 0
+            }
+        ];
+        
     }
 }
 
