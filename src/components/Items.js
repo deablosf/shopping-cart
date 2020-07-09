173213f9641cf81424fd584f0222004b33a246b9
Student_PSXGA136 <deablosf@hotmail.com>
2020-07-09 15:29:00 -0400

    here

---
+++ src/components/Items.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+class Items extends Component {
+    const products = 
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default Items
